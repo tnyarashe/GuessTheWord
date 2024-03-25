ac1e642de3d22a3aa43845737693720fff550443
Tsepo <3tnyarashe@gmail.com>
2024-03-25 15:36:41 +0200

    Guess The Word Program

---
+++ behaviour.js
@@ -0,0 +1,16 @@
+let words = ["CSS", "CODE", "BASH", "FUNCTION", "VARIABLE", "JAVASCRIPT"]
+let bonusWords = ["SHAPER", "AYOBA"]
+
+let randomNum = math.randomNum()
+
+function scrambleWord (word){
+
+}
+
+function wordGenerator(word) {
+    let a;
+    for (i = 0; i < words.length; i++) {
+
+    }
+    return word;
+}
