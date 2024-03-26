6ac1c26e0bb02d4ffc4f1e98ad6ff890bc991dfc
Tsepo <3tnyarashe@gmail.com>
2024-03-26 17:14:30 +0200

    made the scramble word function work

--- behaviour.js
+++ behaviour.js
@@ -4,53 +4,53 @@ const guess = document.getElementById("guessBtn")
 
 const words = ["CSS", "CODE", "BASH", "FUNCTION", "VARIABLE", "JAVASCRIPT"]
 
-let newArray = []
+// let newArray = []
 
-let score = 0;
-let streak = 0;
-let lives = 3;
-let penalty = -1;
+// let score = 0;
+// let streak = 0;
+// let lives = 3;
+// let penalty = -1;
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    scrambledWord.textContent = words[r]
-})
 
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
+
+function shuffleArray(words) {
+    for (var i = words.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        var temp = words[i];
+        words[i] = words[j];
+        words[j] = temp;
     }
-    return array
+    return words
 }
 
-
-
-function shuffleWord(str) { 
-    words = Array.from(str); 
-    for (let i = 0; i < strArray.length - 1; i++) { 
-        let j = Math.floor(Math.random() * strArray.length); 
-        // Swap letters 
-        let temp = strArray[i]; 
-        strArray[i] = strArray[j]; 
-        strArray[j] = temp; 
-    } 
-    return strArray.join(" ");
-} 
-
-guess.addEventListener ("click", ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
+    scrambledWord.textContent = shuffleArray(words)[0]
 })
 
-next.addEventListener ("click", ()=>{
-})
+// function shuffleWord(str) { 
+//     words = Array.from(str); 
+//     for (let i = 0; i < strArray.length - 1; i++) { 
+//         let j = Math.floor(Math.random() * strArray.length); 
+//         // Swap letters 
+//         let temp = strArray[i]; 
+//         strArray[i] = strArray[j]; 
+//         strArray[j] = temp; 
+//     } 
+//     return strArray.join(" ");
+// } 
 
-function refresh() { 
-    index = Math.floor(Math.random() * 6);
-    displayWord = words[index];
-    scrambleWord.innerText = shuffle(displayWord).toUpperCase();
-    document.getElementById("output").innerText = "Result:";
-} 
+// guess.addEventListener ("click", ()=>{
+// })
+
+// next.addEventListener ("click", ()=>{
+// })
+
+// function refresh() { 
+//     index = Math.floor(Math.random() * 6);
+//     displayWord = words[index];
+//     scrambleWord.innerText = shuffle(displayWord).toUpperCase();
+//     document.getElementById("output").innerText = "Result:";
+// } 
   
-//Function call when page load for first time 
-refresh();
+// //Function call when page load for first time 
+// refresh();
